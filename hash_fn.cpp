bb9f6fde0d6322ae690a17b6bb0005552752cda4
s1101046@mail.yzu.edu.tw <s1101046@mail.yzu.edu.tw>
2025-11-23 13:26:46 +0800

    //modify hashStringFuntion

--- hash_fn.cpp
+++ hash_fn.cpp
@@ -20,7 +20,7 @@ int myHashInt(int key, int m) {
 }
 
 int myHashString(const std::string& str, int m) {
-    unsigned long hash = 0;
+    unsigned long hash = str[0] - '0';
     // TODO: replace with your own design
     return static_cast<int>(hash % m);  // basic division method
 }
